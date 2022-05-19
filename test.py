dae5eae8daf70b219b251e80c33eb027486bdf1c
Steve D. J <84221016+Steve235lab@users.noreply.github.com>
2022-05-19 23:37:45 +0800

    将业务代码从excel_database迁移到database

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-# from chat_websocket.database import User, Group, Database, ChatHistory, HistoryMeta
+from chat_websocket.database import User, Group, Database, ChatHistory, HistoryMeta
 #
 # g_1 = Group(['起飞','降落'], 114514, 'g_1')
 #
-# new_user = User(username='小李', password='114514', avatar='../static/avatars/A1.JPG', contacts=['110'],
-#                 invitations=[], email='email.com', confirmed='no', confirm_code='None')
-#
+new_user = User(username='小王', password='114514', avatar='../static/avatars/A1.JPG', contacts=['110'],
+                invitations=[], email='email.com', confirmed='no', confirm_code='None')
+
 # history = HistoryMeta(group_num='12581', sender='校长', content='开除')
-# room1 = Database()
+room1 = Database()
 # name="野兽先辈"
-#room1.add_user(new_user)
+room1.add_user(new_user)
 #room1.add_group(g_1)
 #user = room1.get_user_with_username(name)
 #print(user.contacts)
